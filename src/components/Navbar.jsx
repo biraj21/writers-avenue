64defb4e1d8e194b569b6894f0c3f13546d2d1bb
Biraj <biraj.pub@gmail.com>
2022-10-20 11:22:53 +0530

    navbar improved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,18 +6,20 @@ const activeClassName = "active";
 export default function Navbar({ links }) {
   return (
     <nav className="navbar">
-      <h1>Blogs</h1>
-      <div className="navbar__links">
-        {links.map((link) => (
-          <NavLink
-            to={link.path}
-            key={link.path}
-            className={({ isActive }) => (isActive ? activeClassName : null)}
-            end={link.end}
-          >
-            {link.text}
-          </NavLink>
-        ))}
+      <div className="wrapper">
+        <h1>Blogs</h1>
+        <div className="navbar__links">
+          {links.map((link) => (
+            <NavLink
+              to={link.path}
+              key={link.path}
+              className={({ isActive }) => (isActive ? activeClassName : null)}
+              end={link.end}
+            >
+              {link.text}
+            </NavLink>
+          ))}
+        </div>
       </div>
     </nav>
   );
