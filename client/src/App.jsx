312ea19bbad12203d98ad4953fb843b8b19b5af5
Biraj <biraj.pub@gmail.com>
2023-01-13 15:21:14 +0530

    route protection added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Navbar from "./components/Navbar";
-import Post from "./pages/Post";
+import { authContext } from "./contexts/authContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import Post from "./pages/Post";
 import Register from "./pages/Register";
 import Write from "./pages/Write";
 
@@ -16,12 +18,39 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/posts/:id" element={<Post />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/write" element={<Write />} />
+
+          {/* these routes should only be accessible when the user IS NOT logged in */}
+          <Route element={<OnlyUnauthRoutes redirectTo="/" />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+
+          {/* these routes should only be accessible when the user IS logged in */}
+          <Route element={<AuthRoutes redirectTo="/login" />}>
+            <Route path="/write" element={<Write />} />
+          </Route>
+
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </div>
     </div>
   );
+}
+
+function AuthRoutes({ redirectTo }) {
+  if (!redirectTo) {
+    throw new Error("<AuthRoutes />: prop 'redirectTo' is required!");
+  }
+
+  const { currentUser } = useContext(authContext);
+  return currentUser ? <Outlet /> : <Navigate to={redirectTo} />;
+}
+
+function OnlyUnauthRoutes({ redirectTo }) {
+  if (!redirectTo) {
+    throw new Error("<OnlyUnauthRoutes/>: prop 'redirectTo' is required!");
+  }
+
+  const { currentUser } = useContext(authContext);
+  return currentUser ? <Navigate to={redirectTo} /> : <Outlet />;
 }
