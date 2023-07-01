182b6cd34f60ccb929f7033cc1669e7779b8164e
Biraj <biraj.pub@gmail.com>
2023-07-01 19:51:33 +0530

    improve custom error classes
    - renamed status to statusCode
    - added property statusCode to CustomError itself
    - ValidationError status code changed from 422 to 400

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   if (err instanceof CustomError) {
-    res.status(err.status).json({ error: err.message });
+    res.status(err.statusCode).json({ error: err.message });
   } else {
     console.error(err);
     res.status(500).json({ error: "internal server error" });
--- server/util/error.js
+++ server/util/error.js
@@ -1,30 +1,28 @@
 export class CustomError extends Error {
-  constructor(message) {
+  constructor(message, statusCode = 500) {
     super(message);
     this.isCustom = true;
+    this.statusCode = statusCode;
   }
 }
 
 export class ValidationError extends CustomError {
   constructor(message) {
-    super(message);
+    super(message, 400);
     this.name = this.constructor.name;
-    this.status = 422;
   }
 }
 
 export class ActionForbiddenError extends CustomError {
   constructor() {
-    super("you are not authorized to perform this action");
+    super("you are not authorized to perform this action", 403);
     this.name = this.constructor.name;
-    this.status = 403;
   }
 }
 
 export class AuthError extends CustomError {
   constructor(message) {
-    super(message);
+    super(message, 401);
     this.name = this.constructor.name;
-    this.status = 401;
   }
 }
