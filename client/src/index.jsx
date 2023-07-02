e049f72524c179edf24f55f26c3152c49395018f
Biraj <biraj.pub@gmail.com>
2023-07-02 17:56:52 +0530

    fix: add Bearer in Authorization header

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,7 +11,7 @@ axios.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers["Authorization"] = token;
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
 
     return config;
